c9765831d799266d8c73ced785f89f1f9a7c02c5
WILLIAMS-creator672 <wisdomwills1@outlook.com>
2025-08-12 15:45:25 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let mobileMenuButton = document.getElementById('mobileMenuTrigger')
+let mobileNav = document.getElementById('navBar')
+
+mobileMenuButton.addEventListener('click', () => {
+    mobileNav.classList.toggle('left-0');
+})
+
+
