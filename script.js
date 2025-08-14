3fac5f7c055ff6930046ffe482dc3ab26704d550
WILLIAMS-creator672 <wisdomwills1@outlook.com>
2025-08-14 16:35:40 +0100

    made changes

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
+let overlay = document.getElementById('overlay')
+let hamburgerOverlay = document.getElementById('hamburgerOverlay')
 let mobileMenuButton = document.getElementById('mobileMenuTrigger')
 let mobileNav = document.getElementById('navBar')
 
+let openMenuIcon = mobileMenuButton.querySelectorAll('i')[0]
+let closeMenuIcon = mobileMenuButton.querySelectorAll('i')[1]
+
 mobileMenuButton.addEventListener('click', () => {
     mobileNav.classList.toggle('left-0');
+closeMenuIcon.classList.toggle('scale-0')
+openMenuIcon.classList.toggle('scale-0')
+hamburgerOverlay.classList.toggle('hidden')
 })
+
+hamburgerOverlay.addEventListener('click', () => {
+    mobileNav.classList.toggle('left-0')
+    closeMenuIcon.classList.toggle('scale-0')
+    openMenuIcon.classList.toggle('scale-0')
+    hamburgerOverlay.classList.toggle('hidden')
+})
+
+
 
 
 
@@ -22,8 +39,6 @@ let aboutCampButton = document.querySelectorAll('#programmes button')[1]
 let aboutSpellingButton = document.querySelectorAll('#programmes button')[2]
 
 // LISTENERS
-
-let overlay = document.getElementById('overlay')
 
 aboutPhotograhButton.addEventListener('click', () => {
     programmesAboutModal.classList.toggle('scale-150')
@@ -50,4 +65,32 @@ overlay.addEventListener('click', () => {
     overlay.classList.add('hidden')
    programmesAboutModal.classList.toggle('scale-150')
 })
+
+// PROGRAMME CARDS SLIDE IN EFFECT
+
+// Function to observe when the elements come into view
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            // Add the 'in-view' class to trigger animation
+            entry.target.classList.add('in-view');
+        } else {
+            // Remove the 'in-view' class when the element is out of view
+            entry.target.classList.remove('in-view');
+        }
+    });
+}, {
+    threshold: 0.5, // Trigger when 50% of the element is in view
+    rootMargin: '0px 0px -20% 0px', // Trigger when the element is about to enter the viewport
+});
+
+// Target each programme-card element for the scroll animation
+const leftCard = document.querySelector('.left-card');
+const bottomCard = document.querySelector('.bottom-card');
+const rightCard = document.querySelector('.right-card');
+
+// Observe the cards for when they enter the viewport
+observer.observe(leftCard);
+observer.observe(bottomCard);
+observer.observe(rightCard);
 
